9c175316ea03669461e9993974f574b41d19b4e2
JackieLin2004 <linxy20040304@163.com>
2024-12-15 02:38:54 +0800

    Preliminary completion of front-end and back-end tuning

--- src/App.vue
+++ src/App.vue
@@ -18,28 +18,32 @@
       </div>
       <div>
         <div v-if="isHomePage" class="login-and-register">
-          <el-button plain @click="loginVisible = true">登录</el-button>
-          <el-dialog v-model="loginVisible" width="33em" style="background-color: whitesmoke">
+          <el-button plain @click="loginVisible = true" style="border-color: deepskyblue">登录</el-button>
+          <el-dialog
+            v-model="loginVisible"
+            width="33em"
+            style="background-color: whitesmoke"
+          >
             <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
               <h1 style="color: black; font-size: 2em">登录</h1>
               <div style="font-size: 1em; color: #868484; margin-top: 10px">在进入系统之前，请先输入用户名和密码进行登录</div>
             </div>
             <div class="input-wrapper">
-              <el-form>
+              <el-form :model="login_form" :rules="login_rules" ref="formRef">
                 <el-form-item prop="username">
-                  <el-input maxlength="30" type="text" placeholder="用户名/邮箱">
+                  <el-input v-model="login_form.username" maxlength="30" type="text" placeholder="用户名/邮箱">
                     <template #prefix>
                       <el-icon>
-                        <User />
+                        <User/>
                       </el-icon>
                     </template>
                   </el-input>
                 </el-form-item>
                 <el-form-item prop="password">
-                  <el-input type="password" maxlength="20" style="margin-top: 5px" placeholder="密码">
+                  <el-input v-model="login_form.password" type="password" maxlength="20" style="margin-top: 5px" placeholder="密码">
                     <template #prefix>
                       <el-icon>
-                        <Lock />
+                        <Lock/>
                       </el-icon>
                     </template>
                   </el-input>
@@ -47,7 +51,7 @@
                 <el-row style="margin-top: 5px">
                   <el-col :span="12" style="text-align: left">
                     <el-form-item prop="remember">
-                      <el-checkbox label="记住我" />
+                      <el-checkbox label="记住我"/>
                     </el-form-item>
                   </el-col>
                   <el-col :span="12" style="text-align: right">
@@ -57,7 +61,7 @@
               </el-form>
               <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
                 <div style="margin-top: 1vw">
-                  <el-button style="width: 22vw" type="success" plain @click="changeToLearn2">立即登录</el-button>
+                  <el-button style="width: 22vw" type="success" plain @click="userLogin()">立即登录</el-button>
                 </div>
                 <el-divider>
                   <span style="color: grey; font-size: 0.9em;">没有账号</span>
@@ -68,12 +72,16 @@
               </div>
             </div>
           </el-dialog>
-          <el-dialog v-model="resetVisible" width="33em" style="background-color: whitesmoke">
+          <el-dialog
+            v-model="resetVisible"
+            width="33em"
+            style="background-color: whitesmoke"
+          >
             <div style="text-align: center">
               <div style="margin-top: 1vw">
                 <el-steps :active="active" finish-status="success" align-center>
-                  <el-step title="验证电子邮件" />
-                  <el-step title="重新设定密码" />
+                  <el-step title="验证电子邮件"/>
+                  <el-step title="重新设定密码"/>
                 </el-steps>
               </div>
               <div style="margin: 0 2vw" v-if="active === 0">
@@ -82,12 +90,12 @@
                   <div style="font-size: 1em; color: #868484; margin-top: 10px">请输入需要重置密码的电子邮件地址</div>
                 </div>
                 <div style="margin-top: 1.5vw">
-                  <el-form>
+                  <el-form :model="reset_form" :rules="reset_rules" ref="reset_formRef">
                     <el-form-item prop="email">
-                      <el-input type="email" placeholder="电子邮件地址">
+                      <el-input v-model="reset_form.email" type="email" placeholder="电子邮件地址">
                         <template #prefix>
                           <el-icon>
-                            <Message />
+                            <Message/>
                           </el-icon>
                         </template>
                       </el-input>
@@ -95,17 +103,17 @@
                     <el-form-item prop="code">
                       <el-row :gutter="10" style="width: 100%">
                         <el-col :span="18">
-                          <el-input maxlength="6" type="text" placeholder="请输入验证码">
+                          <el-input v-model="reset_form.code" :maxlength="6" type="text" placeholder="请输入验证码">
                             <template #prefix>
                               <el-icon>
-                                <EditPen />
+                                <EditPen/>
                               </el-icon>
                             </template>
                           </el-input>
                         </el-col>
                         <el-col :span="5">
-                          <el-button type="success">
-                            获取验证码
+                          <el-button @click="reset_askCode" :disabled="!reset_isEmailValid || reset_coldTime > 0" type="success">
+                            {{ reset_coldTime > 0 ? `请稍后 ${reset_coldTime} 秒` : '获取验证码' }}
                           </el-button>
                         </el-col>
                       </el-row>
@@ -122,21 +130,21 @@
                   <div style="font-size: 1em; color: #868484; margin-top: 10px">请填写你的新密码，并牢记防止丢失</div>
                 </div>
                 <div style="margin-top: 1.5vw">
-                  <el-form>
+                  <el-form :model="reset_form" :rules="reset_rules" ref="reset_formRef">
                     <el-form-item prop="password">
-                      <el-input maxlength="20" type="password" placeholder="密码">
+                      <el-input v-model="reset_form.password" maxlength="20" type="password" placeholder="密码">
                         <template #prefix>
                           <el-icon>
-                            <Lock />
+                            <Lock/>
                           </el-icon>
                         </template>
                       </el-input>
                     </el-form-item>
                     <el-form-item prop="password_repeat">
-                      <el-input maxlength="20" type="password" placeholder="重复密码">
+                      <el-input v-model="reset_form.password_repeat" maxlength="20" type="password" placeholder="重复密码">
                         <template #prefix>
                           <el-icon>
-                            <Lock />
+                            <Lock/>
                           </el-icon>
                         </template>
                       </el-input>
@@ -151,45 +159,50 @@
           </el-dialog>
 
           <el-button type="primary" @click="registerVisible = true">注册</el-button>
-          <el-dialog v-model="registerVisible" width="33em" style="background-color: whitesmoke">
-            <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+          <el-dialog
+            v-model="registerVisible"
+            width="33em"
+            style="background-color: whitesmoke"
+          >
+            <div
+              style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
               <h1 style="color: black; font-size: 2em">注册</h1>
-              <div style="font-size: 1em; color: #868484; margin-top: 10px">欢迎注册我们的学习平台，请在下方填写相关信息</div>
+              <div style="font-size: 1em; color: #868484; margin-top: 10px">欢迎注册Medi-Insight网站，请在下方填写相关信息</div>
             </div>
             <div class="input-wrapper">
-              <el-form>
+              <el-form :model="register_form" :rules="register_rules" ref="register_formRef">
                 <el-form-item prop="username">
-                  <el-input maxlength="30" type="text" placeholder="用户名/邮箱">
+                  <el-input v-model="register_form.username" maxlength="30" type="text" placeholder="用户名/邮箱">
                     <template #prefix>
                       <el-icon>
-                        <User />
+                        <User/>
                       </el-icon>
                     </template>
                   </el-input>
                 </el-form-item>
                 <el-form-item prop="password">
-                  <el-input type="password" maxlength="20" style="margin-top: 5px" placeholder="密码">
+                  <el-input v-model="register_form.password" type="password" maxlength="20" style="margin-top: 5px" placeholder="密码">
                     <template #prefix>
                       <el-icon>
-                        <Lock />
+                        <Lock/>
                       </el-icon>
                     </template>
                   </el-input>
                 </el-form-item>
                 <el-form-item prop="password_repeat">
-                  <el-input type="password" maxlength="20" style="margin-top: 5px" placeholder="重复密码">
+                  <el-input v-model="register_form.password_repeat" type="password" maxlength="20" style="margin-top: 5px" placeholder="重复密码">
                     <template #prefix>
                       <el-icon>
-                        <Lock />
+                        <Lock/>
                       </el-icon>
                     </template>
                   </el-input>
                 </el-form-item>
                 <el-form-item prop="email">
-                  <el-input type="email" placeholder="电子邮件地址">
+                  <el-input v-model="register_form.email" type="email" placeholder="电子邮件地址">
                     <template #prefix>
                       <el-icon>
-                        <Message />
+                        <Message/>
                       </el-icon>
                     </template>
                   </el-input>
@@ -197,17 +210,17 @@
                 <el-form-item prop="code">
                   <el-row :gutter="10" style="width: 100%;">
                     <el-col :span="18">
-                      <el-input maxlength="6" type="text" placeholder="请输入验证码">
+                      <el-input v-model="register_form.code" :maxlength="6" type="text" placeholder="请输入验证码">
                         <template #prefix>
                           <el-icon>
-                            <EditPen />
+                            <EditPen/>
                           </el-icon>
                         </template>
                       </el-input>
                     </el-col>
                     <el-col :span="5">
-                      <el-button type="success">
-                        获取验证码
+                      <el-button @click="askCode" :disabled="!isEmailValid || coldTime" type="success">
+                        {{ coldTime > 0 ? `请稍后 ${coldTime} 秒` : '获取验证码' }}
                       </el-button>
                     </el-col>
                   </el-row>
@@ -215,7 +228,7 @@
               </el-form>
               <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
                 <div style="margin-top: 1vw">
-                  <el-button style="width: 22vw" type="warning" plain @click="changeToLogin">立即注册</el-button>
+                  <el-button style="width: 22vw" type="warning" plain @click="register2">立即注册</el-button>
                 </div>
                 <div style="margin: 1vw 0">
                   <span style="font-size: 1em; line-height: 15px; color: grey">已有账号?&nbsp;&nbsp;&nbsp;</span>
@@ -230,7 +243,7 @@
           <template #dropdown>
             <el-dropdown-menu style="width: 20em;">
               <el-dropdown-item>个人信息</el-dropdown-item>
-              <el-dropdown-item style="color: red">退出登录</el-dropdown-item>
+              <el-dropdown-item style="color: red" @click="userLogout">退出登录</el-dropdown-item>
             </el-dropdown-menu>
           </template>
         </el-dropdown>
@@ -246,11 +259,13 @@
 import { register } from 'swiper/element/bundle';
 register();
 import { useRouter, useRoute } from 'vue-router';
-import { computed } from "vue";
+import {computed, reactive} from "vue";
 import { ref } from 'vue'
 import { EditPen, Lock, Message, User } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
 import { useAccountStore } from './store/accountStore';
+import {login, get, post, logout} from "@/net/index.js";
+
 const router = useRouter()
 const accountStore = useAccountStore()
 const role = accountStore.role
@@ -276,53 +291,228 @@ const changeToLearn = () => {
     name: 'learn'
   })
 }
-const changeToLearn2 = () => {
-  if(role == 0){
-    router.push({
-      name: 'learn'
-    })
-  }else{
-    router.push({
-      name: 'aichat'
-    })
-  }
-  ElMessage.success('登录成功，欢迎回来~')
-}
 
 const route = useRoute();
 const isHomePage = computed(() => route.path === '/home')
 
+// ==============登录==================
+const formRef = ref()
+const login_form = reactive({
+  username: '',
+  password: '',
+  remember: false,
+})
+
+const login_rules = {
+  username: [
+    {required: true, message: '请输入用户名'}
+  ],
+  password: [
+    {required: true, message: '请输入密码'}
+  ]
+}
+
+const userLogin = ()=> {
+  formRef.value.validate((isValid) => {
+    if (isValid) {
+      login(login_form.username, login_form.password, login_form.remember, () => {
+        router.push('/learn')
+      })
+    }
+  });
+}
+
+// ==============注册==================
+const register_formRef = ref()
+const coldTime = ref(0)
+
+const register_form = reactive({
+  username: '',
+  password: '',
+  password_repeat: '',
+  email: '',
+  code: ''
+})
+
+const validateUsername = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
+    callback(new Error('用户名不能包含特殊字符，只能是中文/英文'))
+  } else {
+    callback()
+  }
+}
+
+const validatePassword = (rule, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== register_form.password) {
+    callback(new Error("两次输入的密码不一致"))
+  } else {
+    callback()
+  }
+}
+
+const register_rules = {
+  username: [
+    {validator: validateUsername, trigger: ['blur', 'change']},
+    {min: 1, max: 30, message: '用户名的长度必须在1-30个字符之间', trigger: ['blur', 'change']},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 20, message: '密码的长度必须在6-20个字符之间', trigger: ['blur', 'change']}
+  ],
+  password_repeat: [
+    {validator: validatePassword, trigger: ['blur', 'change']},
+  ],
+  email: [
+    {required: true, message: '请输入邮件地址', trigger: 'blur'},
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入获取的验证码', trigger: 'blur'},
+  ]
+}
+
+const askCode =() => {
+  if (isEmailValid) {
+    coldTime.value = 60
+    get(`http://localhost:8088/auth/ask-code?email=${register_form.email}&type=register`, () => {
+      ElMessage.success(`验证码已发送到邮箱: ${register_form.email}，请注意查收`)
+      const handle = setInterval(() => {
+        coldTime.value--
+        if (coldTime.value === 0) {
+          clearInterval(handle)
+        }
+      }, 1000)
+    }, (message) => {
+      ElMessage.warning(message)
+      coldTime.value = 0
+    })
+  } else {
+    ElMessage.warning("请输入正确的电子邮件！")
+  }
+}
+
+const isEmailValid = computed(() => /^[\w.-]+@[\w.-]+\.\w+$/.test(register_form.email))
+
+const register2 =() => {
+  register_formRef.value.validate((isValid) => {
+    if (isValid) {
+      post('http://localhost:8088/auth/register', {...register_form}, () => {
+        ElMessage('注册成功，欢迎加入我们')
+        registerVisible.value = false
+        loginVisible.value = true
+      })
+    } else {
+      ElMessage.warning('请完整填写注册表单内容')
+    }
+  })
+}
+
+// ==============重置密码==================
+const reset_form = reactive({
+  email: '',
+  code: '',
+  password: '',
+  password_repeat: ''
+})
+
+const reset_formRef = ref()
+const reset_coldTime = ref(0)
+const active = ref(0)
+const reset_isEmailValid = computed(() => /^[\w.-]+@[\w.-]+\.\w+$/.test(reset_form.email))
+
+const reset_validatePassword = (rules, value, callback) => {
+  if (value === '') {
+    callback(new Error('请再次输入密码'))
+  } else if (value !== reset_form.password) {
+    callback(new Error("两次输入的密码不一致"))
+  } else {
+    callback()
+  }
+}
+
+const reset_rules = {
+  email: [
+    {required: true, message: '请输入邮件地址', trigger: 'blur'},
+    {type: 'email', message: '请输入合法的电子邮件地址', trigger: ['blur', 'change']}
+  ],
+  code: [
+    {required: true, message: '请输入获取的验证码', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 20, message: '密码的长度必须在6-20个字符之间', trigger: ['blur']}
+  ],
+  password_repeat: [
+    {validator: reset_validatePassword, trigger: ['blur', 'change']},
+  ],
+}
+
+const reset_askCode =() => {
+  if (reset_isEmailValid) {
+    reset_coldTime.value = 60
+    get(`http://localhost:8088/auth/ask-code?email=${reset_form.email}&type=reset`, () => {
+      ElMessage.success(`验证码已发送到邮箱: ${reset_form.email}，请注意查收`)
+      const handle = setInterval(() => {
+        reset_coldTime.value--
+        if (reset_coldTime.value === 0) {
+          clearInterval(handle)
+        }
+      }, 1000)
+    }, (message) => {
+      ElMessage.warning(message)
+      reset_coldTime.value = 0
+    })
+  } else {
+    ElMessage.warning("请输入正确的电子邮件！")
+  }
+}
+
+const confirmReset =() => {
+  reset_formRef.value.validate((isValid) => {
+    if (isValid) {
+      post('http://localhost:8088/auth/reset-confirm', {
+        email: reset_form.email,
+        code: reset_form.code
+      }, () => active.value++)
+    }
+  })
+}
+
+const doReset =() => {
+  reset_formRef.value.validate((isValid) => {
+    if (isValid) {
+      post('http://localhost:8088/auth/reset-password', {...reset_form}, () => {
+        ElMessage.success('密码重置成功，请重新登录')
+        registerVisible.value = false
+        loginVisible.value = true
+      })
+    }
+  })
+}
+
 const loginVisible = ref(false)
 const registerVisible = ref(false)
 const resetVisible = ref(false)
-const changeToLogin = () => {
-  registerVisible.value = false
-  loginVisible.value = true
-  ElMessage.success('注册成功，请登录进入学习平台')
-}
-const changeToLogin2 = () => {
+const changeToLogin2 = ()=> {
   registerVisible.value = false
   loginVisible.value = true
 }
-const changeToRegister = () => {
+const changeToRegister = ()=> {
   loginVisible.value = false
   registerVisible.value = true
 }
-const changeToReset = () => {
+const changeToReset = ()=> {
   loginVisible.value = false
   resetVisible.value = true
 }
 
-const active = ref(0)
-const confirmReset = () => {
-  active.value++
-}
-const doReset = () => {
-  active.value--
-  resetVisible.value = false
-  loginVisible.value = true
-  ElMessage.success('密码重置成功，请重新登录')
-}
+const userLogout = () => {
+  logout(() => router.push('/home'))
+};
 
 </script>
 
